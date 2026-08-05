b424426475104fef2591d304e25151c6ef20e96b
Radwa Medhat <radwamedhat200@gmail.com>
2026-08-05 09:29:27 +0300

    Use @MockitoBean instead of @Mock to properly inject into Spring context

--- src/test/java/org/example/aiinsightservice/services/LlmResilientClientTest.java
+++ src/test/java/org/example/aiinsightservice/services/LlmResilientClientTest.java
@@ -2,13 +2,10 @@ package org.example.aiinsightservice.services;
 
 import org.example.aiinsightservice.dto.InsightResponseDto;
 import org.example.aiinsightservice.exception.LlmServiceException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tools.jackson.databind.ObjectMapper;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -17,7 +14,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class LlmResilientClientTest {
 
-    @Mock
+    @MockitoBean
     private AnthropicApiClient anthropicApiClient;
     @Autowired
     private LlmResilientClient llmResilientClient;
